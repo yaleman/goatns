24bf0bbe00f29fa099c44a19b9d77015058f9f0f
yaleman <yaleman@users.noreply.github.com>
2022-10-31 12:22:59 +0000

    deploy: 61acd90d0259fc05d39b692fe1e8e6400d66d07b

--- rustdoc/source-files.js
+++ rustdoc/source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"goatns":["",[["api",[],["mod.rs"]],["tests",[],["e2e_test.rs","mod.rs"]]],["config.rs","datastore.rs","enums.rs","lib.rs","packet_dumper.rs","reply.rs","resourcerecord.rs","serializers.rs","servers.rs","utils.rs","zones.rs"]]\
+"goatns":["",[["api",[],["mod.rs"]],["db",[],["mod.rs"]],["tests",[],["e2e_test.rs","mod.rs"]]],["config.rs","datastore.rs","enums.rs","lib.rs","packet_dumper.rs","reply.rs","resourcerecord.rs","serializers.rs","servers.rs","utils.rs","zones.rs"]]\
 }');
 createSourceSidebar();
