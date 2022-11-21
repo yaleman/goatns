da6cdcf2c421151cbda99e83e7ecc056ec69088b
yaleman <yaleman@users.noreply.github.com>
2022-11-21 12:37:23 +0000

    deploy: 05b6698541d56460d43f74848214b0e76dca21e5

--- rustdoc/source-files.js
+++ rustdoc/source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"goatns":["",[["db",[],["mod.rs"]],["web",[["auth",[],["mod.rs","sessions.rs","traits.rs"]],["middleware",[],["csp.rs","gunk.rs","mod.rs"]]],["api.rs","generic.rs","macros.rs","mod.rs","ui.rs","utils.rs"]]],["cli.rs","config.rs","datastore.rs","enums.rs","lib.rs","packet_dumper.rs","reply.rs","resourcerecord.rs","serializers.rs","servers.rs","utils.rs","zones.rs"]]\
+"goatns":["",[["db",[],["mod.rs"]],["web",[["api",[],["auth.rs","mod.rs"]],["auth",[],["mod.rs","sessions.rs","traits.rs"]],["middleware",[],["csp.rs","gunk.rs","mod.rs"]],["ui",[],["mod.rs","user_settings.rs"]]],["generic.rs","macros.rs","mod.rs","utils.rs"]]],["cli.rs","config.rs","datastore.rs","enums.rs","lib.rs","packet_dumper.rs","reply.rs","resourcerecord.rs","serializers.rs","servers.rs","utils.rs","zones.rs"]]\
 }');
 createSourceSidebar();
