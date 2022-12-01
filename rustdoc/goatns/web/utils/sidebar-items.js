98cf7491fc2f16e717e96c739d59b392cd42471f
yaleman <yaleman@users.noreply.github.com>
2022-12-01 01:43:53 +0000

    deploy: 7c43e4bff82db7dc62d51283a73e553d8b168364

--- rustdoc/goatns/web/utils/sidebar-items.js
+++ rustdoc/goatns/web/utils/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":[["create_api_token",""],["redirect_to_dashboard",""],["redirect_to_home",""],["redirect_to_login",""],["redirect_to_zones_list",""],["validate_api_token","validate an API token matches our thingamajig"]],"struct":[["ApiToken",""]]};
+window.SIDEBAR_ITEMS = {"fn":[["create_api_token",""],["handler_404",""],["redirect_to_dashboard",""],["redirect_to_home",""],["redirect_to_login",""],["redirect_to_zones_list",""],["validate_api_token","validate an API token matches our thingamajig"]],"struct":[["ApiToken",""]]};
