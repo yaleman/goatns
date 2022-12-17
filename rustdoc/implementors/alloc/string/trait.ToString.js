44666a9ecf4727d9cfdc9b5dcca9eb0d605028ca
yaleman <yaleman@users.noreply.github.com>
2022-12-17 07:40:18 +0000

    deploy: 3f46e1825752092a84529597a9713e53ada67431

--- rustdoc/implementors/alloc/string/trait.ToString.js
+++ rustdoc/implementors/alloc/string/trait.ToString.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"goatns":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"enum\" href=\"goatns/enums/enum.ContactDetails.html\" title=\"enum goatns::enums::ContactDetails\">ContactDetails</a>"]]
+"goatns":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"enum\" href=\"goatns/enums/enum.ContactDetails.html\" title=\"enum goatns::enums::ContactDetails\">ContactDetails</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
