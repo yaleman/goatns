bf525143fabcf9c9952e52c742babebfafe3bdc5
yaleman <yaleman@users.noreply.github.com>
2023-02-21 22:11:04 +0000

    deploy: cdf23e3ef110c9d7347a299d4a3e69c55bb5d2b5

--- rustdoc/goatns/reply/sidebar-items.js
+++ rustdoc/goatns/reply/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":[["reply_any","Reply to an ANY request with a HINFO “RFC8482” “” response"],["reply_builder","Want a generic empty reply with an ID and an RCODE? Here’s your function."],["reply_nxdomain",""]],"struct":[["Reply",""]]};
+window.SIDEBAR_ITEMS = {"fn":[["reply_any","Reply to an ANY request with a HINFO “RFC8482” “” response"],["reply_builder","Want a generic empty reply with an ID and an RCODE? Here’s your function."],["reply_nxdomain","Build a NXDOMAIN response"]],"struct":[["Reply",""]]};
