84d3c1fc32324def5d7150601c8ea4930c24c51b
yaleman <yaleman@users.noreply.github.com>
2023-02-27 21:46:01 +0000

    deploy: fb6f7a6a22717c4a1e830171638446ff5064bae7

--- rustdoc/source-files.js
+++ rustdoc/source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"goatns":["",[["db",[],["mod.rs"]],["web",[["api",[],["auth.rs","filezone.rs","filezonerecord.rs","mod.rs"]],["auth",[],["mod.rs","sessions.rs","traits.rs"]],["doh",[],["mod.rs"]],["middleware",[],["csp.rs","gunk.rs","mod.rs"]],["ui",[],["mod.rs","user_settings.rs"]]],["generic.rs","macros.rs","mod.rs","utils.rs"]]],["cli.rs","config.rs","datastore.rs","enums.rs","lib.rs","packet_dumper.rs","reply.rs","resourcerecord.rs","serializers.rs","servers.rs","utils.rs","zones.rs"]]\
+"goatns":["",[["db",[],["mod.rs"]],["web",[["api",[],["auth.rs","filezone.rs","filezonerecord.rs","mod.rs"]],["auth",[],["mod.rs","sessions.rs","traits.rs"]],["doh",[],["mod.rs"]],["middleware",[],["csp.rs","gunk.rs","mod.rs"]],["ui",[],["admin_ui.rs","mod.rs","user_settings.rs"]]],["generic.rs","macros.rs","mod.rs","utils.rs"]]],["cli.rs","config.rs","datastore.rs","enums.rs","lib.rs","packet_dumper.rs","reply.rs","resourcerecord.rs","serializers.rs","servers.rs","utils.rs","zones.rs"]]\
 }');
 createSourceSidebar();
