6f5fcf7d093f368ebedcb2be2f0454a41130c5d4
yaleman <yaleman@users.noreply.github.com>
2023-12-29 22:49:52 +0000

    deploy: 70b316e061d4808fb8ad153a6d651a4c9b71e1f7

--- rustdoc/implementors/core/str/traits/trait.FromStr.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"goatns":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.1/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"struct\" href=\"goatns/config/struct.ConfigFile.html\" title=\"struct goatns::config::ConfigFile\">ConfigFile</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
