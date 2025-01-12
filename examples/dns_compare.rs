6fdb534b179e60aba5908c8a4ce060a26818edd1
James Hodgkinson <james@terminaloutcomes.com>
2025-01-12 20:06:17 +1000

    fix: dns double checker

--- examples/dns_compare.rs
+++ examples/dns_compare.rs
@@ -1,6 +1,10 @@
+//! Yeah, don't use this with anything but ipv4 and UDP for now.
+//!
+//! This is a simple packet sniffer that listens for DNS packets on a specified port.
+//!
+
 use clap::*;
 use packed_struct::prelude::*;
-// use std::collections::HashSet;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
 const UDP_HEADER_LENGTH: usize = 12;
@@ -21,8 +25,6 @@ struct PacketHeader {
     identification: u16,
     #[packed_field(bits = "56..59", endian = "msb")]
     flags: [bool; 3],
-    // #[packed_field(bits = "59..64", endian = "msb")]
-    // fragment_offset: Vec<bool>,
     #[packed_field(bytes = "8", endian = "msb")]
     ttl: u8,
     #[packed_field(bytes = "9", endian = "msb")]
@@ -37,8 +39,17 @@ impl PacketHeader {
     }
 
     fn ip_data<'a>(&self, data: &'a [u8]) -> &'a [u8] {
-        println!("IP data: {:x?}", &data[12..=self.header_length() - 1]);
         &data[12..=self.header_length() - 1]
+    }
+
+    fn payload<'a>(&self, data: &'a [u8]) -> &'a [u8] {
+        // this differs for TCP when it's the return data?
+        if self.protocol == 17 {
+            &data[self.header_length() + 8..]
+        } else {
+            // panic!("Can't handle returning the payload yet!")
+            &data[self.header_length()..]
+        }
     }
 }
 
@@ -105,6 +116,36 @@ struct UdpPacket {
     checksum: u16,
 }
 
+const TCP_PACKET_HEADER: usize = 26;
+
+#[derive(Debug, PackedStruct)]
+#[packed_struct(bit_numbering = "msb0")]
+struct TcpPacket {
+    #[packed_field(bytes = "0..", endian = "msb")]
+    source_port: u16,
+    #[packed_field(bytes = "4..", endian = "msb")]
+    dest_port: u16,
+    #[packed_field(bytes = "8..", endian = "msb")]
+    sequence_number: u32,
+    #[packed_field(bytes = "16..", endian = "msb")]
+    ack_number: u32,
+
+    #[packed_field(bits = "160..164", endian = "msb")]
+    header_length: u8,
+    #[packed_field(bits = "164..", endian = "msb")]
+    flags: [bool; 12],
+    #[packed_field(bits = "176..", endian = "msb")]
+    window: u16,
+    #[packed_field(bits = "192..", endian = "msb")]
+    checksum: u16,
+}
+
+impl TcpPacket {
+    fn has_data(&self) -> bool {
+        self.flags[2]
+    }
+}
+
 #[tokio::main]
 pub async fn main() {
     let cli = Cli::parse();
@@ -129,7 +170,7 @@ pub async fn main() {
             .expect("slice with incorrect length");
 
         let header = PacketHeader::unpack(&header_slice).expect("Failed to parse packet header");
-        println!("Header: {:x?}", header);
+        // println!("Header: {:x?}", header);
 
         if packet.data.len() != header.length as usize {
             eprintln!(
@@ -138,17 +179,20 @@ pub async fn main() {
                 header.length
             );
         }
-        println!("Header length: {}", header.header_length());
+        // println!("Header length: {}", header.header_length());
 
-        let ip_data = match header.version {
+        let (ip_data, remaining_packets) = match header.version {
             4 => {
                 let mut slice: [u8; 8] = [0; 8];
                 slice.copy_from_slice(header.ip_data(packet.data));
                 let ipv4packet = Ipv4Header::unpack(&slice).expect("Failed to parse DNS packet");
-                IpData {
-                    source: ipv4packet.source_addr(),
-                    dest: ipv4packet.dest_addr(),
-                }
+                (
+                    IpData {
+                        source: ipv4packet.source_addr(),
+                        dest: ipv4packet.dest_addr(),
+                    },
+                    header.payload(packet.data),
+                )
             }
             // 6 => {
             //     let mut slice: [u8; 32] = [0; 32];
@@ -162,14 +206,60 @@ pub async fn main() {
             _ => panic!("Unsupported IP version: {}", header.version),
         };
 
-        // let udp_data = &packet.data[header.header_length()..header.header_length() + 8];
+        let (source_port, dest_port) = match header.protocol {
+            17 => {
+                let mut udp_slice: [u8; 8] = [0; 8];
+                udp_slice.copy_from_slice(
+                    &packet.data[header.header_length()..header.header_length() + 8],
+                );
+                let udp_packet = UdpPacket::unpack(&udp_slice).expect("Failed to parse UDP packet");
 
-        let mut udp_slice: [u8; 8] = [0; 8];
-        udp_slice.copy_from_slice(&packet.data[header.header_length()..header.header_length() + 8]);
-        let udp_packet = UdpPacket::unpack(&udp_slice).expect("Failed to parse UDP packet");
+                // println!("UDP Packet: {:?}", udp_packet);
+                (udp_packet.source_port, udp_packet.dest_port)
+            }
+            6 => {
+                let mut tcp_slice: [u8; TCP_PACKET_HEADER] = [0; TCP_PACKET_HEADER];
+                tcp_slice.copy_from_slice(
+                    &packet.data
+                        [header.header_length()..header.header_length() + TCP_PACKET_HEADER],
+                );
+                let tcp_packet = TcpPacket::unpack(&tcp_slice).expect("Failed to parse TCP packet");
+                if !tcp_packet.has_data() {
+                    // eprintln!("No data in TCP packet");
+                    continue;
+                }
+                println!("TCP Packet: {:?}", tcp_packet);
+                println!("{:x?}", &tcp_slice);
 
-        println!("IP Data: {:?}", ip_data);
+                (tcp_packet.source_port, tcp_packet.dest_port)
+            }
+            _ => {
+                eprintln!("Unsupported protocol: {}", header.protocol);
+                continue;
+            }
+        };
 
-        println!("UDP Packet: {:?}", udp_packet);
+        println!(
+            "{}:{} -> {}:{}",
+            ip_data.source, source_port, ip_data.dest, dest_port
+        );
+
+        // println!("remaining bytes of packet {:x?}", remaining_packets);
+
+        let dns_header: &[u8; 12] = remaining_packets[0..12]
+            .try_into()
+            .expect("slice with incorrect length");
+        let dns_header = match goatns::Header::unpack(dns_header) {
+            Ok(val) => val,
+            Err(err) => {
+                eprintln!("Failed to parse DNS header: {:?}", err);
+                continue;
+            }
+        };
+        println!("DNS Header: {:?}", dns_header);
+
+        println!("\n\n");
+
+        // TODO: dig around and parse the response betterer
     }
 }
